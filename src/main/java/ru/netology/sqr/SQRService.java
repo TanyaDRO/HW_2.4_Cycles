33f10808644032de4365bd03d259ddf66d26bad7
TanyaDRO <drtanja@yandex.ru>
2021-11-20 13:16:16 +0300

    improvement

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,11 +1,14 @@
 package ru.netology.sqr;
 
 public class SQRService {
+    public int beginCounter = 10;
+    public int endCounter = 99;
+
     public int countSquares(int left, int right) {
         int quantity = 0;
-        for (int i = 10; i <= 99; i++) {
+        for (int i = beginCounter; i <= endCounter; i++) {
             int square = i * i;
-            if(square >= left && square <= right) {
+            if (square >= left && square <= right) {
                 System.out.println("Число: " + i + ", квадрат числа: " + square);
                 quantity++;
             }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -6,14 +6,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
     @Test
-    public void countSquares() {
+    public void countSquaresMiddle() {
+        SQRService sQRService = new SQRService();
         int left = 200;
         int right = 300;
-        SQRService sQRService = new SQRService();
         int expected = 3;
         int fact = sQRService.countSquares(left, right);
         assertEquals(expected, fact);
 
     }
 
+    @Test
+    public void countSquaresFullRange() {
+        SQRService sQRService = new SQRService();
+        int left = sQRService.beginCounter * sQRService.beginCounter;
+        int right = sQRService.endCounter * sQRService.endCounter;
+        int expected = sQRService.endCounter - sQRService.beginCounter + 1;
+        int fact = sQRService.countSquares(left, right);
+        assertEquals(expected, fact);
+    }
+
+    @Test
+    public void countSquaresBoundaryValues() {
+        SQRService sQRService = new SQRService();
+        int left = sQRService.beginCounter * sQRService.beginCounter + 1;
+        int right = sQRService.endCounter * sQRService.endCounter - 1;
+        int expected = sQRService.endCounter - sQRService.beginCounter - 1;
+        int fact = sQRService.countSquares(left, right);
+        assertEquals(expected, fact);
+    }
 }
